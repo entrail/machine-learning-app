85113974e07cfd4bf3cfb925d846c9c0d6cd0d88
Adrian horstmann <Adrian@adrians-mbp.speedport.ip>
2017-09-25 21:16:17 +0200

    initialize project

---
+++ machine-learning-app/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  machine-learning-app
+//
+//  Created by Adrian horstmann on 25.09.17.
+//  Copyright © 2017 Adrian Horstmann. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
